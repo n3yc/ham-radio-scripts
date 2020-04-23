1b2488783acc8eb5b5f8cb8642d402054afcc32b
Donald R Anderson <2606216+donaldranderson@users.noreply.github.com>
2020-04-23 14:13:07 -0400

    Update IC-7300-set-frequency.py

--- IC-7300-set-frequency.py
+++ IC-7300-set-frequency.py
@@ -35,7 +35,7 @@
 # =====================================================
 
 # IMPORT MODULES
-import time
+import sys
 import serial
 
 # THE RADIO AND OUR SCRIPT CI_V ADDRESSES
@@ -46,6 +46,12 @@ civ_addr_us = 0xE0 # default
 baudrate = 57600
 device = "/dev/tty.SLAB_USBtoUART" # MacOS IC-7300 USB SERIAL PORT
 
+# GET THE FREQUENCY ARGUMENT
+if len(sys.argv) < 2:
+	print("Usage:", sys.argv[0], "<frequency>")
+	exit(1)
+freq = int(sys.argv[1])
+
 # OPEN THE SERIAL PORT
 port = serial.Serial()
 port.baudrate = baudrate
@@ -53,6 +59,39 @@ port.port = device
 port.timeout = 1
 port.open()
 
+# CREATE THE BCD DIGITS
+bcd = [0x00, 0x00, 0x00, 0x00, 0x00]
+for idx in [0, 1, 2, 3, 4]:
+	bcd[idx] = int(freq % 10)
+	freq /= 10
+	bcd[idx] += int(freq % 10) * 16
+	freq /= 10
+
+# SEND THE PACKET TO REQUEST THE FREQUENCY
+port.write([0XFE, 0XFE, civ_addr_radio, civ_addr_us, 0x05] + bcd +[0xFD])
+
+# GET THE FREQUENCY RESPONSE
+resp = port.read(100)
+
+# WAS IT FOR US?
+if (resp[2] != civ_addr_us):
+	print ("Received a response that was not for us!")
+	port.close()
+	exit(1)
+
+# WAS IT FROM OUR RADIO?
+if (resp[3] != civ_addr_radio):
+	print ("Received a response from another radio!")
+	port.close()
+	exit(1)
+
+# WAS THERE AN ERROR?
+if (resp[4] == 0xFA):
+	print ("Received an error from the radio!")
+	port.close()
+	exit(1)
+else: 
+	print ("Success!")
 
 # CLOSE THE SERIAL PORT
 port.close()
